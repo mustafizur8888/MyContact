7875271550e7c790bb312e3bcf5ba9f06a0d7fe7
Mustafizur Rahman <mustafizur8888@gmail.com>
2016-12-03 21:05:33 +0600

    first letter and favourite done

--- MyContact/Controllers/HomeController.cs
+++ MyContact/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Web.Mvc;
 using MyContact.Models;
+using MyContact.ViewModel;
 
 namespace MyContact.Controllers
 {
@@ -12,17 +15,40 @@ namespace MyContact.Controllers
         public HomeController()
         {
             ViewBag.CountofContact = _db.Contacts.Count();
+            ViewBag.SideBar = "All Contact";
+
         }
 
         public ActionResult Index()
         {
-            return View();
+            var contacts = _db.Contacts.GroupBy(x => x.Name.Substring(0, 1).ToUpper(), (alphabet, x) => new
+            {
+                Alphabet = alphabet,
+                contactList = x.OrderBy(y => y.Name).ToList()
+
+            }).OrderBy(x => x.Alphabet).ToList();
+
+
+            List<ContactViewModel> contactViewModels = contacts.Select(x => new ContactViewModel
+            {
+                FirstAlphabet = x.Alphabet,
+                Contacts = x.contactList
+            }).ToList();
+
+
+
+
+
+
+
+            return View(contactViewModels);
         }
 
         [HttpGet]
         public ActionResult Create()
         {
             Contact contact = new Contact();
+            ViewBag.SideBar = "Add New";
             return View(contact);
         }
 
@@ -54,6 +80,58 @@ namespace MyContact.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            Contact contact = _db.Contacts.Single(x => x.ContactId == id);
+            if (contact == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+
+            return View(contact);
+        }
+
+
+        public ActionResult MakeFavourite(int id)
+        {
+            Contact dbContact = _db.Contacts.FirstOrDefault(x => x.ContactId == id);
+            if (dbContact == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            if (dbContact.Favourite != null && dbContact.Favourite.Value)
+            {
+                dbContact.Favourite = false;
+            }
+            else
+            {
+                dbContact.Favourite = true;
+            }
+            _db.SaveChanges();
+            return View("Details", dbContact);
+        }
+        public ActionResult Favourite()
+        {
+
+
+
+            var contacts = _db.Contacts.Where(x => x.Favourite.Value)
+                .GroupBy(x => x.Name.Substring(0, 1).ToUpper(), (alphabet, x) => new
+                {
+                    Alphabet = alphabet,
+                    contactList = x.OrderBy(y => y.Name).ToList()
+
+                }).OrderBy(x => x.Alphabet);
+
+
+            List<ContactViewModel> contactViewModels = contacts.Select(x => new ContactViewModel
+            {
+                FirstAlphabet = x.Alphabet,
+                Contacts = x.contactList
+            }).ToList();
+            return View("Index", contactViewModels);
         }
     }
 }
---
+++ MyContact/ViewModel/ContactViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MyContact.Models;
+
+namespace MyContact.ViewModel
+{
+    public class ContactViewModel
+    {
+
+        public string FirstAlphabet { get; set; }
+        public List<Contact> Contacts { get; set; }
+    }
+}
